81941c5d394b0953a5cd11a9742bb7071ba77e54
Eduardo Vieira <carlosevhs@gmail.com>
2024-09-25 10:46:51 -0300

    User insertion with POST

--- src/main/java/com/carlosdev/workshopmongo/services/UserService.java
+++ src/main/java/com/carlosdev/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.carlosdev.workshopmongo.domain.User;
+import com.carlosdev.workshopmongo.dto.UserDTO;
 import com.carlosdev.workshopmongo.repository.UserRepository;
 import com.carlosdev.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,22 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
-		}
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+
 }
