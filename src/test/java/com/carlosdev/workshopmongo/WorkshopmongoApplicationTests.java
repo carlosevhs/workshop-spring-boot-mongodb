8629030b8ea2994ebabbc2a00733afe389d9543b
Eduardo Vieira <carlosevhs@gmail.com>
2024-09-25 09:05:29 -0300

    First commit - Created project

---
+++ src/test/java/com/carlosdev/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosdev.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
